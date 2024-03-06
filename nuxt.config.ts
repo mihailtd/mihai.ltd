93516cedb1cad82340594cdbbe105a16a112b447
Mihai Farcas <farcasmihai91@gmail.com>
2024-03-06 23:20:23 +0200

    feat: add app metadata, configured nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,30 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/index.css"],
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  },
+  app: {
+    head: {
+      title: "Mihai @ Let's Talk Dev - Portfolio Website",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: "Mihai's portfolio website",
+        },
+        {
+          name: "keywords",
+          content: "portfolio, website, dev, developer, programmer, youtube",
+        },
+      ],
+    },
+  },
 });
